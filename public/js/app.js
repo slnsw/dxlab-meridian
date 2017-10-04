574466c70f39a9c7a2072b97511a429a6de3592f
Kaho Cheung <kaho@elefanttraks.com>
2017-10-05 10:52:10 +1100

    Added slnsw logo. Added animation of globe.

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,8 @@
 			map: './images/Miranda_Map_Working_Wraparound_Composite2_4000px.jpg',
 			bumpMap:
 				'./images/Miranda_Map_Working_Wraparound_Composite2_4000px_bump.gif',
-			bumpScale: 0.0008
+			bumpScale: 0.0008,
+			content: "Miranda content"
 		},
 		coronelli1: {
 			name: 'Coronelli Terrestrial',
@@ -28,7 +29,8 @@
 			segments: segments,
 			map: './images/10070028-Coronelli-David-Rumsey-3000px.jpg',
 			bumpMap: './images/10070028-Coronelli-David-Rumsey-3000px-bump.jpg',
-			bumpScale: 0.0005
+			bumpScale: 0.0005,
+			content: "Coronelli content"
 		}
 	};
 
@@ -37,17 +39,34 @@
 	  el: '#app',
 	  data: {
 	    items: globeConfigs,
-			globeKey: globeKey
+			globeKey: globeKey,
+			title: 'Test',
+			content: 'Content',
+			isModalOpen: false,
 	  },
 	  methods: {
-	    changeGlobe: function (newGlobeKey) {
-				// Remove old globe, add new globe
-				scene.remove(spheres[this.$data.globeKey]);
+			changeGlobe: function (newGlobeKey) {
+				var oldGlobe = spheres[this.$data.globeKey];
+				var newGlobe = spheres[newGlobeKey];
+
+				newGlobe.material.opacity = 1;
+				oldGlobe.material.transparent = true;
+				newGlobe.material.transparent = true;
+
 				scene.add(spheres[newGlobeKey]);
+
+				TweenLite.to(oldGlobe.material, 0.2, {opacity: 0, onComplete: function() {
+					scene.remove(oldGlobe);
+				}});
+
+				TweenLite.from(newGlobe.material, 0.2, {opacity: 0, delay: 0.2});
 
 				this.$data.globeKey = newGlobeKey;
 				globeKey = newGlobeKey;
-	    }
+	    },
+			toggleModal: function(event) {
+				this.$data.isModalOpen = !this.$data.isModalOpen;
+			}
 	  }
 	})
 
@@ -85,12 +104,20 @@
 
 	webglEl.appendChild(renderer.domElement);
 
+	// console.log(spheres[globeKey].material.opacity);
+	// spheres[globeKey].material.opacity = 0.5;
+	spheres[globeKey].material.transparent = true;
+
+
 	render();
 
 	// Functions
 	function render() {
+
 		controls.update();
 		spheres[globeKey].rotation.y += 0.0005;
+
+		// spheres[globeKey].material.opacity = Math.sin(new Date().getTime() * .0025);
 
 		requestAnimationFrame(render);
 		renderer.render(scene, camera);
@@ -140,4 +167,68 @@
 		}
 		return false;
 	}
+
+	/* Track original opacities */
+	function trackOriginalOpacities(mesh) {
+
+	    var opacities = [],
+	        materials = mesh.material.materials ? mesh.material.materials : [mesh.material];
+	    for (var i = 0; i < materials.length; i++) {
+	         materials[i].transparent = true;
+	         opacities.push(materials[i].opacity);
+	    }
+	    mesh.userData.originalOpacities = opacities;
+	}
+
+	/* Fade mesh */
+	function fadeMesh(mesh, direction, options) {
+	    options = options || {};
+	    // set and check
+	    var current = { percentage : direction == "in" ? 1 : 0 },
+	    // this check is used to work with normal and multi materials.
+	    mats = mesh.material.materials ?
+	             mesh.material.materials : [mesh.material],
+
+	     originals = mesh.userData.originalOpacities,
+	     easing = options.easing || TWEEN.Easing.Linear.None,
+	     duration = options.duration || 2000;
+	    // check to make sure originals exist
+	    if( !originals ) {
+	         console.error("Fade error: originalOpacities not defined, use trackOriginalOpacities");
+	          return;
+	    }
+	    // tween opacity back to originals
+	    var tweenOpacity = new TWEEN.Tween(current)
+	        .to({ percentage: direction == "in" ? 0 : 1 }, duration)
+	        .easing(easing)
+	        .onUpdate(function() {
+	             for (var i = 0; i < mats.length; i++) {
+	                mats[i].opacity = originals[i] * current.percentage;
+	             }
+	         })
+	         .onComplete(function(){
+	              if(options.callback){
+	                   options.callback();
+	              }
+	         });
+	    tweenOpacity.start();
+	    return tweenOpacity;
+	}
+
+	/* How to use */
+	// fade in
+	// fadeMesh(mesh, "in");
+	// // fade out
+	// fadeMesh(mesh, "out");
+	// // fade with options
+	// fadeMesh(mesh, "in", {
+	//
+	//     duration: 11000,
+	//
+	//     easing: TWEEN.Easing.Quintic.InOut,
+	//
+	//     callback : function (){
+	//         console.log("Fade complete");
+	//     }
+	// });
 })();
